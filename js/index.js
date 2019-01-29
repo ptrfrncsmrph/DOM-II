c977dc73afa5195fe253d1c6803d91bc52412973
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-29 15:21:50 -0500

    Adds shadow to navbar on scroll

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+window.onload = () => {
+  const mainNav = document.querySelector(".main-navigation")
+  const mainNavClasses = mainNav.classList
+  document.addEventListener("scroll", () => {
+    pageYOffset !== 0
+      ? mainNav.classList.add("nav-shadow")
+      : mainNav.classList.remove("nav-shadow")
+  })
+}
