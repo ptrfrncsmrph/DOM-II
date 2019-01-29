b31a46e46ad1a4ad3f029973bee1f560cf1038bb
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-29 15:29:16 -0500

    Adds hover color change to main img

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 // Your code goes here
 window.onload = () => {
   const mainNav = document.querySelector(".main-navigation")
-  const mainNavClasses = mainNav.classList
+  const intro = document.querySelector(".intro img")
   document.addEventListener("scroll", () => {
     pageYOffset !== 0
       ? mainNav.classList.add("nav-shadow")
       : mainNav.classList.remove("nav-shadow")
+  })
+  intro.addEventListener("mouseover", () => {
+    intro.classList.toggle("hover-invert")
   })
 }
